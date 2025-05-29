219dece758b9fd1f8aca414798dfad8a609213ab
aletafoya <ale.tufk@gmail.com>
2025-05-29 17:16:00 -0600

    ok

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
-function deploy() {
-    const container = document.getElementById("message");
+let where_am_i = 1;
+
+function next() {
+    if(where_am_i === 5) return;
+    else if(where_am_i === 1) {
+        us();
+        where_am_i = 2;
+    }
+}
+
+function previous() {
+    if(where_am_i === 1) return;
+    else if(where_am_i === 2) {
+        my_message();
+        where_am_i = 1;
+    }
+}
+
+function my_message() {
+    const container = document.getElementById("my_text");
     container.innerHTML = "";
     container.innerHTML = `
-        <div class="nav">
-        <p class="sections_you"><span class="col" id="first_call">Mi mensaje</span></p>
-        <p class="sections_you"><span class="col" id="second_call">Algo sobre ti</span></p>
-        <p class="sections_you"><span class="col" id="third_call">Tu imagen</span></p>
-        <p class="sections_you"><span class="col" id="fourth_call">Recuerdos</span></p>
-        <p class="sections_you"><span class="col" id="fifth_call">Tú en las letras</span></p>
-        <p class="sections_you"><span class="col" id="sixth_call" onclick="melodies()">Melodías</span></p>
-    </div>
-    <div class="content_you" id="content_you">
-        <div class="my_text"></div>
-        <img src="rose.png" alt="my_love" class="rose">
-    </div>
+        <p class="little">Parte de la historia va a parar a las palmas de las manos. Las mías, flacas e indeseables, 
+            cuentan una necesidad; casi siempre moviéndose, creando más arrugas de las habituales, 
+            se desfiguran como olas del mar. Como un ser independiente, siempre buscan a su gemela idéntica, 
+            una réplica suave e inquieta: tu mano. <br> <br>
+            El primer contacto, una inmensidad insondable, crea una patulea de colores, 
+            un tamborileo eterno que representan mis latidos.
+            <br> <br>
+            Tú, que eres luz, que eres esperanza e idilio, luchas por ver la belleza en las cosas. 
+            Eres ese sentimiento de entero deseo por sentirse bien, eres mi raison d’être.
+            
+            <img src="sun.png" alt="my_love" class="rose">
+            <br> <br>
+            Brisia, lo mejor que te puedo dar en la vida no es algo material (aunque también trataré de dártelo), 
+            sino una certeza; esa certeza es tranquilidad, poder decir que nada temes, que vivas la vida enseñando, 
+            como me enseñas a mí, a no tener miedo. Esa certeza es la paz, que llegues a grande y lo único que 
+            desees es revivir lo que experimentaste.
+            <br> <br>
+            <img src="moon.png" alt="moon" class="rose">
+            Por último, quisiera poderte hacer saber lo que haces en la gente, porque, personalmente, en mí sucede. 
+            Que a pesar de ya haber compartido contigo incontables eventos, aún sigo con esa actitud enervante cuando vamos a 
+            crear uno nuevo. Que sepas, Brisia, que cuando estoy solo, cuando nadie parece estar al tanto de mí, 
+            sé que no estoy solo, pues tu simple recuerdo me acompaña, y eso sucederá toda la vida.
+            <br> <br>
+            Mi felicidad está completa desde que te conozco. </p>
     `;
 }
 
+function us() {
+    const container = document.getElementById("my_text");
+    container.innerHTML = "";
+    container.innerHTML = `
+        <div class="us_img">
+            <p class="little"> Nosotros </p>
+
+        </div>
+    `;
+}
 function melodies() {
     const container = document.getElementById("content_you");
     container.innerHTML = "";
@@ -25,7 +65,7 @@ function melodies() {
             <p class="attributes">Tu rostro                                                                 
             <span class="change_song" onclick="go_to_song('https://open.spotify.com/intl-es/track/7v5OpWQsjk8EVecwnHpNIj?si=f2c9932d3615479f')">--></span></p>
             <p class="attributes">Tu voz
-            <span class="change_song" onclick="go_to_song('https://open.spotify.com/intl-es/track/4nBHJVoRtsU2S7ONexL7iC?si=a7ab319232304365')">--></span></p>
+            <span class="change_song" onclick="go_to_song('https://open.spotify.com/intl-es/track/5peA2cq7ptJbJOQqrx4Zpp?si=499110e59193485e')">--></span></p>
             <p class="attributes">Tus ojos
             <span class="change_song" onclick="go_to_song('https://open.spotify.com/intl-es/track/3zgpJfyPtpX38aZUnu4peT?si=dfcc6f67ca1c442c')">--></span></p>
             <p class="attributes">Tu presencia
@@ -40,12 +80,31 @@ function melodies() {
             <span class="change_song" onclick="go_to_song('https://open.spotify.com/intl-es/track/47zglRNpxJfxadAe3C6xQe?si=824051e4eab44004')">--></span></p>
             <p class="attributes">Tu amor
             <span class="change_song" onclick="go_to_song('https://open.spotify.com/intl-es/track/5NQ41wfi4Pykv4eDTlAoIs?si=dd35a92aed594967')">--></span></p>
+            <p class="attributes">Tu cariño
+            <span class="change_song" onclick="go_to_song('https://open.spotify.com/intl-es/track/3ttgpUD3IQaaJBaAbRQCsM?si=c868e0ee978240d0')">--></span></p>
             <p class="attributes">Tú
             <span class="change_song" onclick="go_to_song('https://open.spotify.com/intl-es/track/4x48SRR0MmHwZFqKlN0zRr?si=13f2853e904a4aa2')">--></span></p>
             </div>
     `;
 }
 
+function about_you() {
+    const container = document.getElementById("content_you");
+    container.innerHTML = "";
+    container.innerHTML = `
+        
+    `;
+}
+
+function your_image() {
+    const container = document.getElementById("content_you");
+    container.innerHTML = "";
+    container.innerHTML = `
+        <div class="image">
+            <img src>
+        </div>
+    `;
+}
 function go_to_song(url) {
     window.open(url, "_blank");
 }
